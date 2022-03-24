218f98430d01b431aa4105a3337ac59835be0c6c
Justin Costenbader <justincostenbader@gmail.com>
2022-03-24 11:21:46 -0400

    Schedule on Dashboard

--- LawnHolder/LawnHolder.Web/Startup.cs
+++ LawnHolder/LawnHolder.Web/Startup.cs
@@ -43,6 +43,9 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        var syncfusionKey = _configuration["SyncfusionLicenseKey"];
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncfusionKey);
+
         // Auto-register services implementing IScoped, ITransient, ISingleton (thanks to Georgi Stoyanov)
         Infrastructure.Startup.ConfigureServicesDiscovery(services, typeof(Startup).Assembly, typeof(ApplicationSeederService).Assembly);
 
