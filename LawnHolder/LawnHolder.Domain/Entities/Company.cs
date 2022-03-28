28e103c947d251f70b363ab54b9bc5fe9f009686
Gavan <Gavanquimbybusiness@gmail.com>
2022-03-28 11:33:06 -0400

    Company List

---
+++ LawnHolder/LawnHolder.Domain/Entities/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnHolder.Domain.Entities
+{
+    public class Company
+    {
+        [Key]
+        public string CompanyName { get; set; }
+        public int CompanyAddress { get; set; }
+        public string Zipcode { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int Phone { get; set; }
+        public string UserID { get; set; }
+
+
+    }
+}
